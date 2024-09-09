fbd5415caae818523ea6e2969f3b1e059baed747
Dev theCoxas <168282052+thecoxas@users.noreply.github.com>
2024-09-09 15:08:50 -0300

    Classes Aninhadas
    aula 01 Inner Classes

---
+++ src/com/intermediario/cursojava/innerclass/Externa.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.intermediario.cursojava.innerclass;
+
+/**
+ *
+ * @author Welton
+ */
+public class Externa {
+    private String texto = "Texto externo";
+    
+    public class Interna{
+        private String texto = "Texto interno";
+        
+        public void imprimeTexto(){
+            System.out.println(texto);
+            
+            // consegue ver membros da classe externa
+            System.out.println(Externa.this.texto);
+        }
+    }
+    public static void main(String[] args) {
+        Externa externa = new Externa();
+        Interna interna = externa.new Interna();
+        interna.imprimeTexto();
+    }
+}
---
+++ src/com/intermediario/cursojava/printf/ExemploPrintf2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.intermediario.cursojava.printf;
+
+/**
+ *
+ * @author Welton
+ */
+public class ExemploPrintf2 {
+    public static void main(String[] args) {
+        String nome = "Welton";
+        int idade = 39;
+        double salario = 2200.50;
+        System.out.printf("Nome: %s, Idade %d, Salário %.2f%n", nome,idade,salario);
+        System.out.println("Lucinéia");
+    }
+    
+}
